da6daeb31135675efb4090143314bffb9b8e92df
Magnus Gustafsson <magnus.gustafsson@intergiro.com>
2021-06-24 11:00:57 +0200

    Remove old nav links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,8 +54,6 @@ module.exports = {
         link: '/merchant/',
         items: [
           { text: 'Integration Guide', link: '/merchant/integration-guide/customer-registration' },
-          { text: 'Customer Registration', link: '/merchant/customer-registration' },
-          { text: 'Customer Integration Guide', link: '/merchant/customer-integration' },
           { text: 'Reference', link: '/merchant/reference/customer' },
           { text: 'Customer Page', link: '/merchant/customer-page/' },
         ]
