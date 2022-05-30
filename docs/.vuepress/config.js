03729cc057f7ed81df1ee6e503d1f2ac206416b7
Ivan Ferdelja <ivan.ferdelja@gmail.com>
2022-05-30 17:05:52 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -49,7 +49,7 @@ module.exports = {
         ]
       },
       {
-        text: 'Merchant',
+        text: 'Card Acquiring',
         ariaLabel: 'Merchant Menu',
         link: '/merchant/',
         items: [
